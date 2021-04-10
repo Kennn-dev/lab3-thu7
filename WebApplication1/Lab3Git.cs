ec0e59486da092ddf9a0cc7fa06653213c403ce5
sinhvien <sinhvien>
2021-04-10 10:40:45 +0700

    Them 1 class va push len server edited

--- WebApplication1/Lab3Git.cs
+++ WebApplication1/Lab3Git.cs
@@ -6,10 +6,7 @@ using System.Web;
 namespace WebApplication1
 {
     public class Lab3Git
-    {
-<<<<<<< HEAD
-        String name;
-=======
->>>>>>> 97591201808572d0a4dce9f223557e7dd5b127b7
+    {	
+	String nameEdit;
     }
 }
